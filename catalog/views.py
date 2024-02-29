7e635948611218b78ceea91e51864bcfbcaa6a40
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2024-02-29 23:02:16 +0500

    создана и применена миграция

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,7 @@
+import csv
+import os
+from datetime import datetime
+
 from django.shortcuts import render
 
 
@@ -10,5 +14,13 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'Name: {name}, Phone: {phone}, Message: {message}')
+        with open('data.csv', 'a', newline='') as file:
+            fieldnames = ['Date', 'Name', 'Phone', 'Massage']
+            data = csv.DictWriter(file, fieldnames=fieldnames)
+            file_empty = os.stat('data.csv').st_size == 0
+            if file_empty:
+                data.writeheader()
+            data.writerow({'Date': datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'Name': name, 'Phone': phone,
+                           'Message': message})
+
     return render(request, 'main/contacts.html')
